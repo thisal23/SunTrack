6ac8a4c5ae1cc7141333cf8d35e0fc71de964be3
yashodha-22uom <139736730+yashodha-22uom@users.noreply.github.com>
2025-07-21 02:03:03 +0530

    Update roleMiddleware.js

--- BackEnd/middleware/roleMiddleware.js
+++ BackEnd/middleware/roleMiddleware.js
@@ -7,9 +7,11 @@ const roleMiddleware = (requiredRole) => {
       if (!user) {
         return res.status(404).json({ message: 'User not found' });
       }
+
       if (!user.role || user.role.name !== requiredRole) {
         return res.status(403).json({ message: 'Access denied' });
       }
+
       next();
     } catch (err) {
       res.status(500).json({ message: err.message });
